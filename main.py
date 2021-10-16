4243fa7c2be732433d4185fb05a0fd0679c3f379
CrossChEp <61206698+CrossChEp@users.noreply.github.com>
2021-10-16 20:39:51 +0300

    Update main.py
    adding menu screen instead main container

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 
 class MainContainer(BoxLayout):
     pass
 
+class MenuScreen(Screen):
+    pass
 
-class LogoApp(App):
+class FunctionApp(App):
     def build(self):
-        return MainContainer()
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        Window.size = (700,500)
+        return sm
 
 
 if __name__ == '__main__':
-    LogoApp().run()
+    FunctionApp().run()
