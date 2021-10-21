ac2db06046628dac0722a2abf5943ec9c50ac0bf
CrossChEp <61206698+CrossChEp@users.noreply.github.com>
2021-10-21 13:38:54 +0300

    Bug fix
    Fixing incorrect rendering of graph. Now it's ready to work!

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.graphics import Line, Color
 from typing import Final
-
 from kivy.utils import rgba
 
 
@@ -17,7 +16,12 @@ class Straight(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def draw(self, k, b):
+    def draw(self):
+        db = open('db.txt', 'r')
+        coords = list(db.read().split())
+        db.close()
+        k = float(coords[0])
+        b = float(coords[1])
         with self.canvas:
             Color(1, 1, 1)
             ed = 50
@@ -27,11 +31,11 @@ class Straight(Widget):
             x2 /= ed
             y1 = (k * x1 + b) * -1
             y2 = (k * x2 + b) + 300
-            self.line3 = Line(points=[350, 1000, 350, 0], width=2)
-            self.line2 = Line(points=[0, 220, 1000, 220], width=2)
+            self.y_axis = Line(points=[350, 1000, 350, 0], width=2)
+            self.x_axis = Line(points=[0, 220, 1000, 220], width=2)
             self.line = Line(points=[x1, y1, x2, y2])
-            self.line4 = Line(points=[300, 260, 300, 180])
-            self.line5 = Line(points=[400, 260, 400, 180])
+            self.unit_segment_x_negative = Line(points=[300, 260, 300, 180])
+            self.unit_segment_y_negative = Line(points=[400, 260, 400, 180])
 
 
 class MenuScreen(Screen):
@@ -44,11 +48,8 @@ class LinearFunction(Screen):
 
     def __init__(self, **kw):
         super().__init__(**kw)
-        self.variables = [0, 0, 0, 0]  # y = kx + b
-
 
     def change(self):
-        self.variables = list(self.txt.text.split())
         db = open('db.txt', 'w')
         db.write(self.txt.text)
         db.close()
@@ -56,25 +57,22 @@ class LinearFunction(Screen):
 
 
 class DrawLinearFunctionScreen(Screen):
+    drawButton = ObjectProperty()
 
     def __init__(self, **kw):
         super().__init__(**kw)
-        db = open('db.txt', 'r')
-        coords = list(db.read().split())
-        db.close()
         self.straight_line = Straight()
         self.add_widget(self.straight_line)
-        self.straight_line.draw(float(coords[0]), float(coords[1]))
 
+    def drawingWindow(self):
+        self.drawButton.text = ''
+        self.straight_line.draw()
 
-
+        
 class FunctionApp(App):
     txt = ObjectProperty()
 
     def build(self):
-        # db = open('db.txt', 'w')
-        # db.write('0 0')
-        # db.close()
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(LinearFunction(name='linear_function'))
@@ -85,4 +83,3 @@ class FunctionApp(App):
 
 if __name__ == '__main__':
     FunctionApp().run()
- 
