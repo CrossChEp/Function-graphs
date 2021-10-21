743e4de416d9b0deb8359c8ff57fdd556b5053c3
CrossChEp <61206698+CrossChEp@users.noreply.github.com>
2021-10-21 14:02:05 +0300

    Moving menu class into seperate file

---
+++ classes/menu_screen.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang.builder import Instruction
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.graphics import Line, Color
+from typing import Final
+from kivy.utils import rgba
+
+class MenuScreen(Screen):
+    pass
